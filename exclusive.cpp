9c277392e7caf0aa46c8e55345edb136a79afc99
swa34 <sa69508@uga,edu>
2021-06-21 22:16:10 -0400

    Better but not working

---
+++ exclusive.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+int exclusive = 0;
+    int numbers = 0;
+    cout << "Enter integers: ";
+    getline( cin,str);
+    stringstream  number;
+    number << str;
+   number >> numbers;
+   exclusive = exclusive ^ numbers;
+    cout << str << endl;
+
+    cout << exclusive << endl;
+
+
+    return 0;
+}//end of main func
